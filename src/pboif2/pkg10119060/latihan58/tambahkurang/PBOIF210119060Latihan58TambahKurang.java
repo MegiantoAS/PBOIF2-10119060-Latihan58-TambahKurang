4971b7d4c6fc7aeb9289338a0c56eb7a9ff841b4
asus <asus@LAPTOP-BD2P4KVT>
2020-11-18 20:51:38 +0700

    new commit

---
+++ src/pboif2/pkg10119060/latihan58/tambahkurang/PBOIF210119060Latihan58TambahKurang.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119060.latihan58.tambahkurang;
+
+/**
+ *
+ @author Megianto Adi saputra
+ Nama         : Megianto Adi Saputra
+ Kelas        : IF-2
+ NIM          : 10119060
+ Deskripsi    : Program Tambah kurang
+ */
+public class PBOIF210119060Latihan58TambahKurang {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JumlahBilangan objJumlahBilangan = new JumlahBilangan();
+        System.out.print("Hasil Perjumlahan = ");objJumlahBilangan.tampilHasilJumlah();
+        
+        SelisihBilangan objSelisihBilangan = new SelisihBilangan();
+        System.out.print("Hasil Selisih 3 - 4 = ");objSelisihBilangan.tampilSelisih();
+    }
+    
+}
---
+++ src/pboif2/pkg10119060/latihan58/tambahkurang/SelisihBilangan.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119060.latihan58.tambahkurang;
+
+/**
+ *
+ @author Megianto Adi saputra
+ Nama         : Megianto Adi Saputra
+ Kelas        : IF-2
+ NIM          : 10119060
+ Deskripsi    : Program Tambah Kurang
+ */
+public class SelisihBilangan extends Bilangan{
+    public void tampilSelisih(){
+        System.out.println(""+(getX()- getY()));
+    }
+    
+}
